7029f8aad6fbcaf6b7f85e410a9c537c596d2789
buueno <90141423+buueno@users.noreply.github.com>
2022-09-21 21:57:48 -0300

    Delete callback.js

--- callback.js
+++
@@ -1,8 +0,0 @@
-const nomescle = ['ana', 'ju', 'leo', 'paula']
-
-nomes.forEach(imprimenomes)             //call back são funções que chama outras funções
-                                        // e pode ser usado posteriormente para ser usado em qualquer array, facilitando
-
-function imprimenomes(nome) {           // escrever a função fora do foreach é bom para usar a mesma em varios locais
-    console.log(nome)
-}
