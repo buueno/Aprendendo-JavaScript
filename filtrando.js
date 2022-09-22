ecb5b5ffc058bbed9e2257872074c1c4c856bf3d
buueno <90141423+buueno@users.noreply.github.com>
2022-09-21 21:58:34 -0300

    Delete filtrando.js

--- filtrando.js
+++
@@ -1,8 +0,0 @@
-const nomes = ['ana', 'marcos', 'maria', 'mauro']
-const notas = [7, 4.5, 8, 7.5]
-
-const reprovados = nomes.filter((alunos,indice) => notas[indice] < 5)  //o parametro alunos nao esta sendo usado
-                                                                      //poderia ser substituido por _ 
-                                                                      // o nome aparece pq filtrer esta associado com nomes 
-
-console.log(`reprovados: ${reprovados}`)
