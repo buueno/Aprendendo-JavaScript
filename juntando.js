b38330792cdc9b1a78c43d51e97de689b75a98de
buueno <90141423+buueno@users.noreply.github.com>
2022-09-21 21:58:46 -0300

    Delete juntando.js

--- juntando.js
+++
@@ -1,9 +0,0 @@
-//juntando duas arrays com concat
-
-const SaladePython = ['melissa', 'helana', 'rodrigo']
-
-const SaladeJS = ['ju', 'leo', 'raquel']
-
-const Salasunificadas = SaladePython.concat(SaladeJS) //nao salva o resultado na array, precisa de outra const (salasunificadas)
-
-console.log(Salasunificadas)
